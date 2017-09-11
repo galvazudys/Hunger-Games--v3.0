cc9ce4700316013d667c23b06a0de382ffe4e48a
galvazudys <golfas705@yahoo.com>
2017-09-11 17:02:12 +0100

    added function to see user list and total calories

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let inpNode = document.getElementById('search');
 let renderList = document.getElementById('list');
+let totalList = document.getElementById('totalList');
+let totalCaloriesNode =document.getElementById('totalCalories');
+let totalCalories = 0;
+let renderTotalList = []
 let listOfFood = [];
 
 fetchData = val => {
+    renderList.innerHTML = '';
     fetch(`https://nutritionix-api.p.mashape.com/v1_1/search/${val}?fields=nf_calories,item_name,brand_name`, {
         method: 'GET',
         headers: {
@@ -20,5 +25,18 @@ fetchData = val => {
     });
 }
 
+renderList.addEventListener('click', (e) => {
+    listOfFood.forEach((item) => {
+        if (e.target.id === item._id) {
+            renderTotalList.push({name: item.fields.item_name, id: item._id, brand: item.fields.brand_name, calories: item.fields.nf_calories});
+            totalCalories += item.fields.nf_calories;
+            totalList.innerHTML = '';
+            renderTotalList.forEach((item) => {
 
+                totalList.innerHTML += `<li id=${item.id}>${item.name} (${item.brand}) /  Calories: ${item.calories}</li>`;
+            })
+            totalCaloriesNode.innerHTML = `Total calories: ${totalCalories}`
+        }
 
+    })
+})
